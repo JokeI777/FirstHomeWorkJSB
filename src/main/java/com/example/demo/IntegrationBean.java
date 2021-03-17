4d80802d90ca3c4f1e8a2e3b3448a5f26e500008
JokeI777 <danleo.2000@mail.ru>
2021-03-17 19:09:27 +0500

    First home work

---
+++ src/main/java/com/example/demo/IntegrationBean.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+public class IntegrationBean {
+    public PlugInterface plug;
+
+    public IntegrationBean(PlugInterface plug){
+        this.plug = plug;
+    }
+
+}
